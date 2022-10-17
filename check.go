86bba6add20ab80583a27368a83eedf3c783de38
Radosław Winiecki <r.winiecki@outlook.com>
2022-10-17 18:21:43 +0200

    try basic scrape

--- check.go
+++ check.go
@@ -3,19 +3,40 @@ package main
 import (
 	"fmt"
 	"github.com/aws/aws-lambda-go/lambda"
+	"github.com/gocolly/colly"
 )
 
 type MyEvent struct {
-	Name string `json:"What is your name?"`
-	Age  int    `json:"How old are you?"`
+	Site string `json:"site"`
 }
 
 type MyResponse struct {
-	Message string `json:"Answer:"`
+	Message string `json:"message"`
 }
 
 func HandleLambdaEvent(event MyEvent) (MyResponse, error) {
-	return MyResponse{Message: fmt.Sprintf("%s is %d years old!", event.Name, event.Age)}, nil
+	c := colly.NewCollector(
+		colly.AllowedDomains("hackerspaces.org", "wiki.hackerspaces.org"),
+	)
+
+	// On every a element which has href attribute call callback
+	c.OnHTML("a[href]", func(e *colly.HTMLElement) {
+		link := e.Attr("href")
+		// Print link
+		fmt.Printf("Link found: %q -> %s\n", e.Text, link)
+		// Visit link found on page
+		// Only those links are visited which are in AllowedDomains
+		c.Visit(e.Request.AbsoluteURL(link))
+	})
+
+	// Before making a request print "Visiting ..."
+	c.OnRequest(func(r *colly.Request) {
+		fmt.Println("Visiting", r.URL.String())
+	})
+
+	// Start scraping on https://hackerspaces.org
+	c.Visit("https://hackerspaces.org/")
+	return MyResponse{Message: fmt.Sprintf("You wanted to scrape %s, so there you go...", event.Site)}, nil
 }
 
 func main() {
