be4475cc735e5ee7d1fd8c463dd62907fc042a43
Radosław Winiecki <r.winiecki@outlook.com>
2022-11-06 22:08:04 +0100

    beautify deploy script

--- deploy.ps1
+++ deploy.ps1
@@ -1,16 +1,32 @@
-Set-Location $PSScriptRoot
-if (Test-Path .\function.zip)
+function Timestamp-Log($Text)
 {
-    Remove-Item .\function.zip
+    $Timestamp = Get-Date -Format HH:mm:ss.fff
+    $Text = $Text.toUpper()
+    Write-Host "[$Timestamp] ============== $Text =============="
 }
-$env:GOARCH="amd64"
-$env:GOOS="linux"
+
+Set-Location $PSScriptRoot
+
+Timestamp-Log("tests")
+go test
+if ($LASTEXITCODE -ne 0)
+{
+    Write-Output "There are test failures"
+    exit 1
+}
+
+Timestamp-Log("build")
+$env:GOARCH = "amd64"
+$env:GOOS = "linux"
 go build -o check check.go
 
+Timestamp-Log("upload to bucket")
 Compress-Archive -Path .\check -DestinationPath .\function.zip
 Remove-Item .\check
 
 aws s3 cp .\function.zip s3://radswn-lambda-bucket
-$FileVersion = (aws s3api list-object-versions --bucket radswn-lambda-bucket| ConvertFrom-Json).Versions[0].VersionId
+Remove-Item .\function.zip
 
-aws cloudformation deploy --stack-name lambda-stack --template-file .\lambda.yaml --capabilities CAPABILITY_NAMED_IAM --parameter-overrides LambdaCodeVersion=$FileVersion
+Timestamp-Log("stack update")
+$FileVersion = (aws s3api list-object-versions --bucket radswn-lambda-bucket| ConvertFrom-Json).Versions[0].VersionId
+aws cloudformation deploy --stack-name lambda-stack --template-file .\lambda.yaml --capabilities CAPABILITY_NAMED_IAM --parameter-overrides LambdaCodeVersion = $FileVersion
