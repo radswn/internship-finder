60bbd2ec3380723cad4d3fb52ec7a082586d75bf
Radosław Winiecki <r.winiecki@outlook.com>
2023-02-22 21:32:10 +0100

    implement Amazon scraping

--- checker/checker.go
+++ checker/checker.go
@@ -7,6 +7,8 @@ import (
 	"github.com/aws/aws-sdk-go-v2/config"
 	"github.com/aws/aws-sdk-go-v2/service/sqs"
 	"github.com/gocolly/colly"
+	"io"
+	"net/http"
 	"os"
 	"strconv"
 	"strings"
@@ -19,8 +21,20 @@ type SqsApi struct {
 }
 
 type Event struct {
-	AppleSite string `json:"appleSite"`
-	Date      string `json:"date"`
+	AmazonSite string `json:"amazonSite"`
+	AppleSite  string `json:"appleSite"`
+	Date       string `json:"date"`
+}
+
+type AmazonJobs struct {
+	Jobs []AmazonJob `json:"jobs"`
+}
+
+type AmazonJob struct {
+	Location   string `json:"normalized_location"`
+	JobPath    string `json:"job_path"`
+	Title      string `json:"title"`
+	PostedDate string `json:"posted_date"`
 }
 
 type Response struct {
@@ -59,14 +73,31 @@ func getSqsApi() SqsApi {
 	}
 }
 
-func isOfferDateRelevant(offerDate string, requestedDate string) bool {
+func isOfferDateRelevant(offerDate string, requestedDate string, dateFormat string) bool {
+	if requestedDate == "all" {
+		return true
+	}
+
+	var current time.Time
+
 	if requestedDate == "" {
-		current := time.Now()
+		current = time.Now()
+	} else {
+		var err error
+		current, err = time.Parse("02.01", requestedDate)
+		if err != nil {
+			panic("error while parsing date" + err.Error())
+		}
+	}
 
-		currentDay := strconv.Itoa(current.Day())
-		currentMonthShort := current.Month().String()[:3]
+	currentDay := strconv.Itoa(current.Day())
+	currentMonth := current.Month().String()
 
-		return strings.Contains(offerDate, currentMonthShort+" "+currentDay)
+	switch dateFormat {
+	case "apple":
+		requestedDate = currentMonth[:3] + " " + currentDay
+	case "amazon":
+		requestedDate = currentMonth + " " + currentDay
 	}
 
 	return strings.Contains(offerDate, requestedDate)
@@ -93,15 +124,22 @@ func collectAllOffers(event Event) ([]Offer, error) {
 		}
 	}
 
+	if event.AmazonSite != "" {
+		amazonOffers, err := getAmazonOffers(event)
+		if err == nil {
+			offers = append(offers, *amazonOffers...)
+		}
+	}
+
 	return offers, err
 }
 
 func getAppleOffers(event Event) (*[]Offer, error) {
-	c := colly.NewCollector(colly.AllowedDomains("jobs.apple.com"))
+	c := colly.NewCollector()
 	offers := make([]Offer, 0)
 
 	c.OnHTML("td.table-col-1", func(e *colly.HTMLElement) {
-		if !isOfferDateRelevant(e.ChildText("span.table--advanced-search__date"), event.Date) {
+		if !isOfferDateRelevant(e.ChildText("span.table--advanced-search__date"), event.Date, "apple") {
 			return
 		}
 
@@ -122,8 +160,49 @@ func getAppleOffers(event Event) (*[]Offer, error) {
 	return &offers, err
 }
 
+func getAmazonOffers(event Event) (*[]Offer, error) {
+	offers := make([]Offer, 0)
+
+	res, err := http.Get(event.AmazonSite)
+	if err != nil {
+		panic("error while GETting Amazon website" + err.Error())
+	}
+
+	resBody, err := io.ReadAll(res.Body)
+	if err != nil {
+		panic("error reading response body" + err.Error())
+	}
+
+	var data AmazonJobs
+
+	err = json.Unmarshal(resBody, &data)
+	if err != nil {
+		panic("error unmarshalling json" + err.Error())
+	}
+
+	for _, j := range data.Jobs {
+		if !isOfferDateRelevant(j.PostedDate, event.Date, "amazon") {
+			continue
+		}
+
+		offer := Offer{
+			Title:    j.Title,
+			Link:     constructLink(event.AmazonSite, j.JobPath),
+			Location: j.Location,
+		}
+
+		offers = append(offers, offer)
+	}
+
+	return &offers, err
+}
+
 func constructLink(visitedUrl string, href string) (url string) {
-	baseUrlEndIdx := strings.Index(visitedUrl, ".com") + 4
+	baseUrlEndIdx := strings.Index(visitedUrl, ".jobs") + 5
+
+	if baseUrlEndIdx == 4 {
+		baseUrlEndIdx = strings.Index(visitedUrl, ".com") + 4
+	}
 	baseUrl := visitedUrl[:baseUrlEndIdx]
 	return baseUrl + href
 }
